cd08d51f2ede116831d6ac77e37098dd21a7f6f0
symmetricalboy <dylan.g.singer@gmail.com>
2025-01-15 12:39:09 -0600

    relative directory addressing fix for prisma import

--- src/pages/api/.well-known/atproto-did.ts
+++ src/pages/api/.well-known/atproto-did.ts
@@ -1,5 +1,5 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
-import { prisma } from "~/server/db";
+import { prisma } from "../../../server/db";
 
 export default async function handler(
   req: NextApiRequest,
